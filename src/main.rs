272a13fd0892de186ec0658383e39a4b1864b67a
Rayan <rayandebruin@gmail.com>
2025-11-23 18:06:07 +0100

    Added Queen's move generation

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,16 @@ mod print_board;
 mod read_board;
 
 fn main() {
-    
+    print_all_piece_moves(move_generation::rook::generate_moves);
+    print_all_piece_moves(move_generation::queen::generate_moves);
+}
+
+fn print_all_piece_moves(move_generator: fn(u64) -> u64) {
+    let starting_position = 0b1u64;
+
+    for i in 0..64 {
+        let current_position = starting_position << i;
+        let piece_possible_moves = move_generator(current_position);
+        print_board::print_possible_moves(piece_possible_moves, current_position);
+    }
 }
